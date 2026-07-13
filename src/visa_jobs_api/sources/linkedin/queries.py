5c3f7bae71c8fc5eb1e34b5ccfb8fe416ee228a7
Shubham arora <shubham.arora.player@gmail.com>
2026-07-13 09:21:50 +0530

    Update queries.py

--- src/visa_jobs_api/sources/linkedin/queries.py
+++ src/visa_jobs_api/sources/linkedin/queries.py
@@ -24,7 +24,7 @@ from __future__ import annotations
 
 from visa_jobs_api.sources.linkedin.models import SearchQuery
 
-DEFAULT_KEYWORDS = "(Python OR Backend OR Java) AND (sponsor OR sponsorship OR "work authorization")"
+DEFAULT_KEYWORDS = '(Python OR Backend OR Java) AND (sponsor OR sponsorship OR "work authorization")'
 
 _COUNTRIES = [
     "United States",
